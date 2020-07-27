3ee1cb3d7407a4dd4714603ae30537352a922446
csd-oss <mk@buialo.com>
2020-07-28 00:50:13 +0300

    basic logic count

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import streamlit as st
 initial_investment: float = 20000.00
 years: int = 10
 return_rate: float = 6.00
-additioan_contribution: float = 100.00
 
+# additioan_contribution: float = 100.00
 
-st.write('dick')
+accrued_amount = initial_investment * (1 + return_rate/100)**years
+
+st.write(accrued_amount)
