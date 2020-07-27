217e5f3a26c7b268dc6be0c40fcc883785575893
csd-oss <mk@buialo.com>
2020-07-28 01:20:02 +0300

    extra text added

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-initial_investment: float = st.sidebar.number_input('Initial investment')
-years: int = st.sidebar.slider('Years', min_value=1, max_value=50)
-return_rate: float = st.sidebar.number_input('Return rate')
-
-# additioan_contribution: float = 100.00
+initial_investment: float = st.sidebar.number_input(
+    'Initial investment',
+    value=10000.00)
+years: int = st.sidebar.slider('Years', min_value=1, max_value=50, value=10)
+return_rate: float = st.sidebar.number_input('Return rate', value=6.00)
 
 accrued_amount = initial_investment * (1 + return_rate/100)**years
 
-st.write(accrued_amount)
+st.write(f'''If you invest {initial_investment} for {years} years with
+return rate of {return_rate} at the end of that period
+you will get {accrued_amount}''')
